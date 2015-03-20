0fb8d7ace5aaa64374c367f0f015c28107f2441c
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2015-03-20 09:28:51 +0300

    canvas add

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,8 @@
-use strict;
 $(function(){
-  console.log('\'Allo \'Allo!');
+    var c = document.getElementById('canvas');
+    console.log(c);
+    var ctx = c.getContext('2d');
+    var img = new Image();
+    img.src = '/images/2013.jpg';
+    ctx.drawImage(img, 0, 0, 300, 561, 0, 0, 300, 561);
 });
