f6106bd2f4a6207e4a25521597d2da81739d5018
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2015-03-21 16:55:48 +0300

    optimize

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,25 @@
 $(function(){
 
+    var pastDelta = true;
+
     var draw = function(delta){
-      var c = document.getElementById('canvas');
-      var ctx = c.getContext('2d');
-      var img1 = new Image();
-      img1.src = '/images/1915.jpg';
-      ctx.drawImage(img1, 0, 0, 848, 561, 0, 0, 848, 561);
-      var img = new Image();
-      img.src = '/images/2013.jpg';
-      ctx.drawImage(img, 0, 0, 848 * (delta), 561, 0, 0, 848 * ( delta), 561);
+        var c = document.getElementById('canvas');
+        var ctx = c.getContext('2d');
+        if(pastDelta || pastDelta > delta){
+            var img1 = new Image();
+            img1.src = '/images/1915.jpg';
+            ctx.drawImage(img1, 0, 0, 848, 561, 0, 0, 848, 561);
+        }
+        var img = new Image();
+        img.src = '/images/2013.jpg';
+        ctx.drawImage(img, 0, 0, 848 * (delta), 561, 0, 0, 848 * ( delta), 561);
+        pastDelta = delta;
     };
 
     draw(0);
 
     var position = $('canvas').position();
     $('canvas').on("mousemove", function( event ) {
-          draw((event.pageX - position.left) / 848);
+        draw((event.pageX - position.left) / 848);
     });
 });
